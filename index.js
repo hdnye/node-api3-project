ca6593ae23246a959c23b7cec568248dfa1563b6
Heather Nye <heatherdnye@gmail.com>
2020-04-13 20:51:27 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code away!
+// Imports
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./posts/userRouter');
+const server = express();
+const port = 5000;
+
+//Middleware Chain
+server.use(express.json());
+server.use(cors());
+server.use(morgan());
+
+//Route Error Handler
+server.use((req, res) => {
+    res.status(404).json({
+        message: 'This route cannot be found',
+    })
+})
+//Server Error Handler
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: 'Request not found',
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server started at http:localhost:${port}`)
+})
